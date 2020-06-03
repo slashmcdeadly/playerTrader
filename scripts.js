28ad640f4eec72da49723f18496c912a99075110
Scott Carter <scottandrewcarter1@hotmail.com>
2020-06-02 23:54:57 -0400

    a basic trade function is in place, minor changes to html.

--- scripts.js
+++ scripts.js
@@ -1,111 +1,149 @@
-// globally scoped variables
 
-const team = {
-    scottsmen: {}, 
-    bedtimePlayers: {}, 
-    hartFoundation: {}, 
-    communist: {}, 
-    goons: {}, 
-    pluss: {}, 
-    hotelSeattle: {}, 
-    dogs: {}, 
-    timbersnakes: {}, 
-    vultures: {}, 
-    thrashers: {}, 
-    backes: {}, 
-    raccoons: {}, 
-    internets: {}
-};
+$(function(){
 
-let scottsmen, bedtimePlayers, hartFoundation, communist, goons, pluss, hotelSeattle, dogs, timbersnakes, vultures, thrashers, backes, raccoons, internets = {
-    players: {}, 
-    manager: {}, 
-    prestige: '', 
-    buildStatus: ''
-};
+    tradeApp.init();
+    
+});
 
-const players = {
-    positionPlayer: {}, 
-    goalie: {}
-};
+const tradeApp = {};
 
-const positionPlayer = {
-    playerName: '',
-    shootingPercentage: 0,
-    shotsPerGame: 0,
-    hitsPerGame: 0,
-    blocksPerGame: 0,
-    shotsPerSixty: 0,
-    hitsPerSixty: 0,
-    ppTime: 0,
-    ppPoints: 0,
-    shPoints: 0,
-    ppg: 0,
-    shp: 0,
-    goals: 0,
-    assists: 0,
-    pointsPerSixty: 0,
-    potential: 0,
-    age: 0,
-    playerPosition: '',
-    playsTopLine: true,
-    isDefenceman: false,
-    totalTradeScore: 0
+// init function
+tradeApp.init = function() {
+
+    let playerLeft, playerRight = {};
+    let leftTradeScore = 0;
+    let rightTradeScore = 0;
+
+    // conditional evaluate button creation
+    
+    const evaluateButton = () =>  {
+        if ($('#displayPlayerLeft').hasClass('submitted') && $('#displayPlayerRight').hasClass('submitted')) {
+            $('#evaluateTrade').html('<button type="submit" id="evaluate" class="evaluate" name="evaluate"> evaluate </button>');}
+    };
+    
+    // link up the submit buttons
+    $("form").on("submit", function(event){
+
+        // prevent the default functionality of submit
+        event.preventDefault();
+        
+        // scoped variables
+        let fullTradeScoreLeft = 0;
+        let fullTradeScoreRight = 0;
+        
+        // make the left player submit functionality
+        $('.playerLeft button').on('click', function(){
+            
+            // store the values from the inputs
+            // Left Player object
+            playerLeft = {
+                name : $('#nameLeft').val(),
+                gpg : $('#gpgLeft').val(),
+                apg : $('#apgLeft').val(),
+                hpg : $('#hpgLeft').val(),
+                spg : $('#spgLeft').val(),
+            };
+            
+            // disable button after submitting
+            $('.playerLeft button').prop('disabled', true);
+            
+            // stat weight function
+            const tradeScore = () => {
+                const weightedGpg = playerLeft.gpg * 11;         
+                const weightedApg = playerLeft.apg * 7;
+                const weightedSpg = playerLeft.spg * 1;
+                const weightedHpg = playerLeft.hpg * 1;
+                
+                fullTradeScoreLeft = (weightedGpg + weightedHpg + weightedSpg + weightedApg) / 2;
+
+                return fullTradeScoreLeft;
+            }
+            
+            // display player name and trade score
+            const displayLeft = $("#displayPlayerLeft");
+            const tradeScoreLeft = tradeScore();
+            
+            displayLeft.html(`<h2> ${playerLeft.name} </h2>`);
+            displayLeft.append(`<p> ${tradeScoreLeft} </p>`);
+            displayLeft.addClass('submitted');
+
+            evaluateButton();
+
+        });
+        
+        // right player submit functionality
+        $('.playerRight button').on('click', function(){
+            // Right Player object
+            playerRight = {
+                name : $('#nameRight').val(),
+                gpg : $('#gpgRight').val(),
+                apg : $('#apgRight').val(),
+                hpg : $('#hpgRight').val(),
+                spg : $('#spgRight').val()
+            };
+
+            // disable button after submitting
+            $('.playerRight button').prop('disabled', true);
+            
+            // stat weight function
+            const tradeScore = () => {
+                const weightedGpg = playerRight.gpg * 11;         
+                const weightedApg = playerRight.apg * 7;
+                const weightedSpg = playerRight.spg * 1;
+                const weightedHpg = playerRight.hpg * 1;
+                
+                fullTradeScoreRight = (weightedGpg + weightedHpg + weightedSpg + weightedApg) / 2;
+
+                return fullTradeScoreRight;
+            }
+            
+            // display player name and trade score
+            const displayRight = $("#displayPlayerRight");
+            const tradeScoreRight = tradeScore();
+            
+            displayRight.html(`<h2> ${playerRight.name} </h2>`);
+            displayRight.append(`<p> ${tradeScoreRight} </p>`);
+            displayRight.addClass('submitted');
+
+            // // conditional evaluate button creation
+            // if ($(displayRight).hasClass('submitted') && $  ('#displayPlayerLeft').hasClass('submitted')) {
+            //     $('#evaluateTrade').html('<button type="submit" id="evaluate" class="evaluate" name="evaluate"> evaluate </button>');
+            // };
+
+            evaluateButton();
+                    
+        });
+        
+        // left and right player comparison function
+        $('#evaluateTrade').on('click', 'button', function(){
+            
+            leftTradeScore = $('#displayPlayerLeft p').text()
+            rightTradeScore = $('#displayPlayerRight p').text();
+
+            const tradeStatus = $('.tradeStatus');
+            const reset = $('.reset');
+
+            const tradeDifference = leftTradeScore - rightTradeScore;
+
+            // if the trade difference is within 5% accept trade
+            if (tradeDifference <= 0.25 && tradeDifference >= -0.25){
+                tradeStatus.html('<h3> trade accepted!!! </h3>');
+                //display reset button
+                reset.html('<button class = "resetButton"> reset </button>')
+            } else {
+                tradeStatus.html('<h3> trade rejected :( </h3>');
+                //display reset button
+                reset.html('<button class = "resetButton"> reset </button>')
+            }
+        })
+
+        // reset button function
+        $('.reset').on('click', 'button', function(){
+            location.reload();
+        })
+    });
 }
 
-const goalie = {
-    playerName: '',
-    shotsPerGame: 0,
-    goalsAgainst: 0,
-    games: 0,
-    wins: 0,
-    shutouts: 0, 
-    totalTradeScore: 0
-}
+        
 
-const manager = {
-    tradeWillingness: 0,
-    draftStrategy: '',
-    managerName: ''
-}
-
-
-// Scottsmen Team Data 
-
-let plDubois = {
-    playerName: 'Pierre Luc Dubois',
-    shootingPercentage: 11.4,
-    shotsPerGame: 2.26,
-    hitsPerGame: 1.21,
-    blocksPerGame: 0.71,
-    ppTime: 160,
-    shPoints: 0,
-    ppp: 10,
-    goals: 18,
-    assists: 31,
-    potential: 85,
-    age: 22,
-    playerPosition: 'center',
-    playsTopLine: true,
-    isDefenceman: false,
-    totalTradeScore: tradeScore()
-}
-
-let bPoint = {
-    playerName: 'Brayden Point',
-    shootingPercentage: 17.7,
-    shotsPerGame: 2.14,
-    hitsPerGame: 0.36,
-    blocksPerGame: 0.45,
-    ppTime: 197,
-    shPoints: 0,
-    ppp: 13,
-    goals: 25,
-    assists: 39,
-    potential: 100,
-    age: 23,
-    playerPosition: 'center',
-    playsTopLine: true,
-    isDefenceman: false,
-    totalTradeScore: tradeScore()
-}
+            
